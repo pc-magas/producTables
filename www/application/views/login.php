a373c539f6cfd0c7cdf09f85ce550d0b6355c96e
Dimitrios Desyllas (pc_magas) <you@example.com>
2017-04-03 22:01:53 +0300

    Login page ready

--- www/application/views/login.php
+++ www/application/views/login.php
@@ -3,8 +3,13 @@
 	$this->load->helper('form');
 ?>
 <div class="login-box">
+	<div class="login-logo">
+    	<p><span class="glyphicon glyphicon-shopping-cart"></span>&nbsp;ProducTables&nbsp;<span class="glyphicon glyphicon-shopping-cart"></span></p>
+  	</div>
 	<div class="login-box-body">
 		<?=form_open('login/login',['method'=>'post']);?>
+			<p class="login-box-msg">Sign in to start your session</p>
+			
 			<div class="form-group has-feedback">
 				<?=form_input(['name'=>'username','class'=>'form-control','placeholder'=>'Username']); ?>
 				<span class="glyphicon glyphicon-user form-control-feedback"></span>
@@ -12,7 +17,12 @@
 			<div class="form-group has-feedback">
 				<?=form_password(['name'=>'password','class'=>'form-control','placeholder'=>'Password']); ?>
 				<span class="glyphicon glyphicon-lock form-control-feedback"></span>
-			</div>      
+			</div>
+			<div class="row">
+				<div class="col-xs-4">
+          			<button type="submit" class="btn btn-primary btn-block btn-flat">Sign In</button>
+        		</div>
+			</div>
 		<?=form_close();?>
 	</div>
 </div>
