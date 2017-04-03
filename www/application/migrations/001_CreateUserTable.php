ceaa8484d40748e130c9c08d7b09b8402169cd8d
Dimitrios Desyllas (pc_magas) <you@example.com>
2017-04-03 22:57:59 +0300

    Add Migration Script

---
+++ www/application/migrations/001_CreateUserTable.php
@@ -0,0 +1,38 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Migration_CreateUserTable extends CI_Migration
+{
+	private $table='user';
+	
+	public function up()
+	{
+		$fields=[
+					'id'=>[
+							'type'=>'INT',
+							'unsigned' => TRUE,
+							'auto_increment' => TRUE
+						  ],
+				 	'username'=>[
+				 				  'type'=>'VARCHAR',
+				 				  'constraint'=>30,
+				 				  'null'=>false
+				 				],
+					'password'=>[
+									'type'=>'VARCHAR',
+									'constraint'=>200,
+									'null'=>false
+								]
+				];
+		
+		$this->dbforge->add_field($fields);
+		$this->dbforge->add_key('id',true);
+		$this->dbforge->create_table($this->table);
+	}
+	
+	public function down()
+	{
+		$this->dbforge->drop_table($this->table);
+	}
+}
