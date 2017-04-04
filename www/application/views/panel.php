30e10efee43afcf2fd0492bdbe44623755c6ff2b
Dimitrios Desyllas (pc_magas) <you@example.com>
2017-04-04 18:29:41 +0300

    Uncommited Commits

--- www/application/views/panel.php
+++ www/application/views/panel.php
@@ -2,11 +2,17 @@
 
 	$this->load->helper('url');
 	$headerViewParamsArray=[
-													'bodyClasses'=>['hold-transition','skin-green','layout-top-nav'],
-													'title'=>'Admin Panel',
-													'stylesheets'=>[base_url('/assets/css/datatables.min.css')],
-													'scripts'=>[base_url('/assets/js/datatables.js'),base_url('/index.php/panel/javascript')]
-												];
+								'bodyClasses'=>['hold-transition','skin-green','layout-top-nav'],
+								'title'=>'Admin Panel',
+								'stylesheets'=>[base_url('/assets/css/datatables.min.css')],
+								'scripts'=>[
+										base_url('/assets/js/fastclick.js'),
+										base_url('/assets/js/jquery.dataTables.min.js'),
+										base_url('/assets/js/datatables.bootstrap.min.js'),
+										base_url('/assets/js/app.min.js'),
+										base_url('/index.php/panel/javascript'),	
+								]
+							];
 
 	$this->load->view('header.php',$headerViewParamsArray);
 
@@ -27,12 +33,12 @@
 			        <div class="navbar-custom-menu">
 			        	<ul class="nav navbar-nav">
 			        		<li class="dropdown user user-menu">
-			        			<a class="dropdown-toggle" href="#" data-toggle="dropdown" aria-expanded="true">
-			        				<span class="user-image"><span class="glyphicon glyphicon-user"></span></span>
-			        				<span>Welcome <?=$username?></span>
+			        			<a id="userDropDown" href="#" class="dropdown-toggle" data-toggle="dropdown">
+                					<img src="https://images.duckduckgo.com/iu/?u=https%3A%2F%2Ftse4.mm.bing.net%2Fth%3Fid%3DOIP.3iumuUC5lngNt-nt7xylQQEsEs%26pid%3D15.1&f=1" class="user-image" alt="User Image">
+			        				<span class="hidden-xs">Welcome <?=$username?></span>
 			        			</a>
 
-				        		<ul class="dropdown-menu">
+				        		<ul class="dropdown-menu" aria-labelledby="userDropDown">
 				        			<li class="user-footer"><a class="btn btn-link" href="<?=base_url('index.php/user/logout');?>" style="color:#d9534f">Logout</a></li>
 				        		</ul>
 			        		</li>
